e33241362f4c17e788c647a12a29571f3bda20cd
flow2000 <1982989137@qq.com>
2022-09-01 22:26:06 +0800

    Site updated: 2022-09-01 22:25:51

--- js/blog.panghai.top.js
+++ js/blog.panghai.top.js
@@ -2,8 +2,10 @@ fetch('https://v6-widget.51.la/v6/JnHw7yNkc1XsvRUB/quote.js').then(res => res.te
     let title = ['最近活跃访客', '今日访问人数', '今日访问量', '昨日人数', '昨日访问量', '本月访问量', '总访问量']
     let num = data.match(/(?<=<\/span><span>).*?(?=<\/span><\/p>)/g)
     let s = document.querySelectorAll('#visitor .content')[0]
-    for (let i = 0; i < num.length; i++) {
-        if (i == 3 || i == 4) continue;
-        s.innerHTML += '<div><p><span id=name>' + title[i] + '</span><span class="num">' + num[i] + '</span></p></div>'
-    }
+	if(s !== undefined){
+		for (let i = 0; i < num.length; i++) {
+			if (i == 3 || i == 4) continue;
+			s.innerHTML += '<div><p><span id=name>' + title[i] + '</span><span class="num">' + num[i] + '</span></p></div>'
+		}
+	}
 });
