084bdbeda6b57b055cc5fe8d7febc3fe86e17052
flow2000 <1982989137@qq.com>
2022-10-07 20:09:03 +0800

    Site updated: 2022-10-07 20:08:48

--- js/blog.panghai.top.js
+++ js/blog.panghai.top.js
@@ -3,20 +3,7 @@ function sleep(time) {
 	return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-// 定义引入js函数
-function addScript(url) {
-	var script = document.createElement('script');
-	script.setAttribute('type', 'text/javascript');
-	script.setAttribute('src', url);
-	document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-var host = window.location.host;
-if (host === "blog.panghai.top") {
-	addScript("https://sdk.51.la/perf/js-sdk-perf.min.js")
-	addScript("https://sdk.51.la/js-sdk-pro.min.js")
-	new LingQue.Monitor().init({ id: "JnC2AbgRbb5Tqrw7", sendSuspicious: true, sendSpaPv: true });
-	LA.init({ id: "JnHw7yNkc1XsvRUB", ck: "JnHw7yNkc1XsvRUB" })
+if (window.location.host === "blog.panghai.top") {
 	var el = document.createElement("script"); 
 	el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?829f1e973776a573fabb58087ddf02bd8c82ad65127a8c20a59ade1867826777bc434964556b7d7129e9b750ed197d397efd7b0c6c715c1701396e1af40cec962b8d7c8c6655c9b00211740aa8a98e2e"; 
 	el.id = "ttzz"; 
